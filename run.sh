354ceec5f859b32bdab7502aa9de4db9a91a0b65
Onni Salminen <osalmine@c2r1p10.hive.fi>
2019-11-08 16:43:00 +0200

    modified .sh to fclean libft

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ clang -Wall -Wextra -Werror -I libft/includes -o get_next_line.o -c get_next_lin
 clang -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
 clang -o test_gnl main.o get_next_line.o -I libft/includes -L libft/ -lft
 rm *.o
+make -C libft/ fclean
